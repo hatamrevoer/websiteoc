176921f379974344173e1379c02d2e2c2384467b
datacvriskypratama <data.cvriskypratama@gmail.com>
2021-07-14 11:25:31 +0800

    All Commit

---
+++ index.php
@@ -0,0 +1,139 @@
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,600;1,800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="assets/img/logo.png" rel="icon">
+        <title>OC Website</title>
+    </head>
+    <style>
+        body{
+            font-family: 'Nunito', sans-serif;
+            font-weight: bold;
+        }
+        .img1{
+            margin-top:3px;
+            height:150px;
+            width:135px;
+            border-radius:30px;
+            border:black solid;
+        }
+        .img2{
+            width:135px;
+            height:150px;
+            margin-top:3px;
+            border-radius:30px;
+            border:black solid;
+        }
+    </style>
+    <body>
+        <!-- NAVBAR ODY CRAFT -->
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color:gold;border-radius:30px;border:black solid;box-shadow: -5px 5px 2px black;padding: 10px;">
+                <a class="navbar-brand" href="#" style="margin-left:15px;">ODYCRAFT</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="box-shadow: -5px -3px 2px black;padding: 10px;margin-right:16px;">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto" style="margin-right:10px;">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php" style="margin-left:15px;">BERANDA</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.facebook.com/profile.php?id=100068514830286" style="margin-left:15px;">FACEBOOK</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://topminecraftservers.org/" style="margin-left:15px;">TOP MINECRAFT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="margin-left:15px;">DISABLED</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+            <!-- END NAVBAR ODYCRAFT -->
+
+            <!-- JUMBOTRON -->
+            <div class="container">
+                <div class="h-100 p-5 text-dark" style="box-shadow: 5px -3px 2px black;padding: 10px;background-color:pink;border-radius:30px;border:black solid;margin-top:10px;">
+                    <h2><img src="assets/img/logo.png" title="odycraft" width="115px"> ODYCRAFT | WEBSITE <form class="form-floating">
+                        <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com" value="IP Server: odycraft.mc-alias.com" style="background-color: pink; width: 25%;border:pink solid;">
+                        <label for="floatingInputValue"></label>
+                        </form></h2>
+                    <p>SOSIAL MEDIAKU!.</p>
+                    <i style="font-size:24px" class="fa" onclick="window.location.href = 'https://www.facebook.com/profile.php?id=100068514830286';">&#xf09a;</i>
+                    <i style="font-size:24px" class="fa" onclick="window.location.href = 'https://github.com/hatamrevoer';">&#xf09b;</i>
+                    <i style="font-size:24px" class="fa" onclick="window.location.href = 'https://www.instagram.com/golda_s4ns/';">&#xf16d;</i>
+                    <!-- tempat icon -->
+                </div>
+            </div>
+            <!-- END JUMBOTRON -->
+
+            <!-- About -->
+            <div class="container">
+                <div class="h-100 p-5 text-dark text-center" style="box-shadow: -5px -3px 2px black;padding: 10px;background-color:rgb(75, 82, 75);border-radius:30px;border:black solid;margin-top:5px;">
+                    <h4 style="color:black;">TENTANG OC!</h4>
+                    <p style="color:black;">ODY CRAFT ADALAH SERVER GAME MINECRAFT YANG SUDAH TERSEDIA SEJAK TAHUN 2017.<br>SERVER ODY CRAFT HANYA BISA DIMAINKAN DI MINECRAFT JAVA KOMPUTER ATAU LAPTOP!</p>
+                </div>
+            </div>
+            <!-- END ABOUT -->
+
+            <!-- Support -->
+            <div class="container">
+                <div class="h-100 p-5 text-dark text-center" style="box-shadow: 5px 5px 2px black;padding: 10px;background-color:rgb(159, 167, 114);border-radius:30px;border:black solid;margin-top:1px;">
+                    <h4 style="color:black;margin-bottom:25px;">SUPPORT</h4>
+                    <i style="font-size:35px" class="fa" title="Minecraft">&#xf1b3;</i>
+                    <i style="font-size:35px" class="fa" title="Data">&#xf0ae;</i>
+                    <i style="font-size:35px" class="fa" title="wifi">&#xf1eb;</i>
+                    <i style="font-size:35px" class="fa" title="Lan">&#xf0e8;</i>
+                    <i style="font-size:35px" class="fa" title="Server">&#xf233;</i>
+                    <i style="font-size:35px" class="fa" title="Chip">&#xf2db;</i>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="h-100 p-5 text-dark text-center" style="box-shadow: -5px 5px 2px black;padding: 10px;background-color:rgb(47, 89, 206);border-radius:30px;border:black solid;margin-top:6px;">
+                    <h4 style="color:black;margin-bottom:25px;">DAFTAR RANK</h4>
+                    <img src="https://dunb17ur4ymx4.cloudfront.net/packages/images/e6d6709c969b73397cd84cf77c96fa3619284d85.png" title="VIP">
+                    <h4>VIP Rank [Lifetime]</h4>
+                    <h2><button class="btn btn-danger">BELI SEKARANG</button></h2>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="h-100 p-5 text-dark text-center" style="box-shadow: 8px -8px 2px black;padding: 10px;background-color:rgb(72, 138, 75);border-radius:30px;border:black solid;margin-top:6px;">
+                    <h4 style="color:black;margin-bottom:25px;">CEO & OWNER</h4>
+                    <img src="assets/img/profil.png" title="VIP" width="225">
+                    <h4>Ahmad Akbar Khatami Asapil</h4>
+                    <p>Menjadi seseorang yang pantas buat seseorang!</p>
+                    <h2><button class="btn btn-warning" style="box-shadow:0 0.375em 0 currentColor;border-style:solid;border-radius:10px;border-color:black solid;border:black solid;" onclick="window.location.href = 'https://www.facebook.com/profile.php?id=100068514830286';">HUBUNGI SEKARANG</button></h2>
+                </div>
+            </div>
+            <!-- END -->
+
+            <!-- Footer -->
+            <div class="container">
+                <div class="h-100 p-5 text-dark text-center" style="box-shadow: -8px -8px 2px black;padding: 10px;background-color:rgb(50, 149, 206);border-radius:30px;border:black solid;margin-top:1px;">
+                    <h2 style="text-shadow: -12px -12px 3px white;font-weight: bold;">© COPYRIGHT ODYCRAFT❤️ ALL RIGHT RESERVED.</h2>
+                </div>
+            </div>
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        -->
+    </body>
+    </html>

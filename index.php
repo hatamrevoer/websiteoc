aa4403c9c9abcdd6d5d74652d3763d7cce85d645
hatamrevoer <69011593+hatamrevoer@users.noreply.github.com>
2021-08-10 20:24:39 +0700

    All Commit

--- index.php
+++ index.php
@@ -38,6 +38,15 @@
     </style>
 
     <body style="background-image:url('https://images.alphacoders.com/490/thumb-1920-490190.jpg')">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1234567890123456" crossorigin="anonymous"></script>
+<!-- Homepage Leaderboard -->
+<ins class="adsbygoogle"
+style="display:inline-block;width:728px;height:90px"
+data-ad-client="ca-pub-1234567890123456"
+data-ad-slot="1234567890"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 </script>
         <!-- NAVBAR ODY CRAFT -->
         <div class="container">

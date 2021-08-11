d3f7ad79213450652eb205269f06899a41c850c7
hatamrevoer <69011593+hatamrevoer@users.noreply.github.com>
2021-08-11 15:36:24 +0700

    All Commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link href="https://lh3.googleusercontent.com/proxy/fG3ZS7qGoG4xILwRiBGqCTs1XSpNo7rv4igWMY7eMXYXQ5qWFGH2bjtViPDmGsdzhZRf5i7lwRXRW24Yx-NXFwig_04xcx_4o_g9Rrvl0csJ6yd43pplocyky0j9vTamXlE82jHU34s5UA" rel="icon">
+        <link href="https://cdn.freebiesupply.com/logos/large/2x/pc-engine-logo-png-transparent.png" rel="icon">
         <title>PREMAN-CHEATERS</title>
     </head>
     <style>
@@ -68,7 +68,7 @@
         <!-- JUMBOTRON -->
         <div class="container">
             <div class="h-100 p-5 text-dark" style="box-shadow: 5px -3px 2px black;padding: 10px;background-color:pink;border:black solid;margin-top:10px;">
-                <h2><img src="https://lh3.googleusercontent.com/proxy/fG3ZS7qGoG4xILwRiBGqCTs1XSpNo7rv4igWMY7eMXYXQ5qWFGH2bjtViPDmGsdzhZRf5i7lwRXRW24Yx-NXFwig_04xcx_4o_g9Rrvl0csJ6yd43pplocyky0j9vTamXlE82jHU34s5UA" title="odycraft" width="75px" style="border-radius:10px; <form class="form-floating">
+                <h2><img src="https://cdn.freebiesupply.com/logos/large/2x/pc-engine-logo-png-transparent.png" title="odycraft" width="75px" style="border-radius:10px; <form class="form-floating">
                     <div style="background: transparent; border: 2px solid #00000000; color: #7CFC00; font-size: 18px; font-weight: bold; letter-spacing: 0.5em; padding: 15px;">
 <marquee direction="left" scrollamount="5" align="center">
 <style typecss="[{http://galihgardhany.blogspot.com}]">
